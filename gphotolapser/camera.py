7966ec4018186f95e766322fe26149bc2633b52b
Tarvi Verro <tarvi.verro@gmail.com>
2018-04-15 12:14:42 +0200

    Adjust for gphoto2 get-config change.

--- gphotolapser/camera.py
+++ gphotolapser/camera.py
@@ -3,13 +3,13 @@ from gph import gph_cmd
 def camera_settings_get():
     # Grab camera configs for aperture, iso and shutter.
     aperture_cfg=gph_cmd('get-config /main/capturesettings/aperture')
-    aperture=[ camera_setting_convert(a) for a in aperture_cfg[3:] ]
+    aperture=[ camera_setting_convert(a) for a in aperture_cfg[4:-1] ]
 
     iso_cfg=gph_cmd('get-config /main/imgsettings/iso')
-    iso=[ camera_setting_convert(i) for i in iso_cfg[3:] ]
+    iso=[ camera_setting_convert(i) for i in iso_cfg[4:-1] ]
 
     shutter_cfg=gph_cmd('get-config /main/capturesettings/shutterspeed')
-    shutter=[ camera_setting_convert(s) for s in shutter_cfg[3:] ]
+    shutter=[ camera_setting_convert(s) for s in shutter_cfg[4:-1] ]
     return (aperture, iso, shutter)
 
 def camera_setting_convert(choice):
